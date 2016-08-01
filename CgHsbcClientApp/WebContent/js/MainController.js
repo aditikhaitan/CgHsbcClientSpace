7ef182f4e7ece88f55629d82af142416fd260390
learning <learning@DIN66007851.corp.capgemini.com>
2016-08-01 15:02:26 +0530

    updated

--- CgHsbcClientApp/WebContent/js/MainController.js
+++ CgHsbcClientApp/WebContent/js/MainController.js
@@ -20,7 +20,7 @@
 	 console.log($scope.messageBody1);
 	}
 	 $scope.initi();
-	 $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded; charset=utf-8; Access-Control-Allow-Origin:* ";
+	 $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded; charset=utf-8; Access-Control-Allow-Origin:http://cghsbcclientspace.mybluemix.net ";
 	 var theObject = { input:"" ,client_id:$scope.client_id,conversation_id: $scope.conversationID};
 	 
 	 
